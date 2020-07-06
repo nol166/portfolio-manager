cc5dfc15e84a1094e9cc7ec61cecff7a7a2219ed
John McCambridge <jmccamb@gmail.com>
2020-07-06 16:21:50 -0500

    refactor in develop branch

--- app.ts
+++ app.ts
@@ -1,16 +1,38 @@
 import { writeFile } from 'fs'
 import { generatePage } from './src/page-template'
+import { prompt } from 'inquirer'
+import { userQuestions } from './src/userQuestions'
+import { projectQuesions } from './src/projectQuestions'
 const profileDataArgs = process.argv.slice(2, process.argv.length)
 const [userName, github] = profileDataArgs
 
-const writeToFile = writeFile(
-    'index.html',
-    generatePage(userName, github),
-    err => {
-        if (err) throw err
+const promptUser = () => {
+    return prompt(userQuestions)
+}
 
-        console.log(
-            'Portfolio complete! Check out index.html to see the output!'
-        )
+const promptProject = (portfolioData: { projects: any[] }) => {
+    if (!portfolioData.projects) {
+        portfolioData.projects = []
     }
-)
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `)
+    return prompt(projectQuesions).then(
+        (projectData: { confirmAddProject: boolean }) => {
+            portfolioData.projects.push(projectData)
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData)
+            } else {
+                return portfolioData
+            }
+        }
+    )
+}
+
+promptUser()
+    .then(promptProject)
+    .then((portfolioData: any) => {
+        console.log(portfolioData)
+    })
---
+++ src/userQuestions.ts
@@ -0,0 +1,17 @@
+export const userQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+    },
+]
