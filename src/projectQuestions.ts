452fbecb97442948e625b2830055771d95d3a5e6
John McCambridge <jmccamb@gmail.com>
2020-07-06 19:30:03 -0500

    add validation methods for questions

--- src/projectQuestions.ts
+++ src/projectQuestions.ts
@@ -3,11 +3,13 @@ export const projectQuesions = [
         type: 'input',
         name: 'name',
         message: 'What is the name of your project?',
+        validate: projectName => (projectName ? true : false),
     },
     {
         type: 'input',
         name: 'description',
         message: 'Provide a description of the project (Required)',
+        validate: projectDesc => (projectDesc ? true : false),
     },
     {
         type: 'checkbox',
@@ -27,6 +29,7 @@ export const projectQuesions = [
         type: 'input',
         name: 'link',
         message: 'Enter the GitHub link to your project. (Required)',
+        validate: link => (link ? true : false),
     },
     {
         type: 'confirm',
--- src/userQuestions.ts
+++ src/userQuestions.ts
@@ -2,16 +2,26 @@ export const userQuestions = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?',
+        message: 'What is your name? (Required)',
+        validate: nameInput => (nameInput ? true : false),
     },
     {
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub Username',
+        validate: github => (github ? true : false),
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message:
+            'Would you like to enter some information about yourself for an "About" section?',
+        default: true,
     },
     {
         type: 'input',
         name: 'about',
         message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => confirmAbout,
     },
 ]
